53dd7f60961a70815c85a0b5a8b48070eac22fcc
maheiniya <118219707+maheiniya@users.noreply.github.com>
2022-12-04 12:51:40 -0500

    Add files via upload

---
+++ learn-spring-boot/src/main/java/com/mahi/springboot/learnspringboot/TestController.java
@@ -0,0 +1,17 @@
+package com.mahi.springboot.learnspringboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class TestController {
+	
+	
+	@RequestMapping(value = "/")
+
+	public String hello() {
+		return "Hello World!!";
+	}
+	
+}
